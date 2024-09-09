eb4becacd475ec9e2f0347996459f98e8867e151
DARL9UAM <dalrodriguez@uamv.edu.ni>
2024-09-09 10:06:15 -0600

    Practica1y2

---
+++ Practica1/Practica1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cp = 0;
+            int cip = 0;
+            const int tam = 10;
+            int[] num = new int[tam];
+            Console.WriteLine("Digite los datos del arreglo");
+            Console.WriteLine();
+            for (int i = 0; i < tam; i++) {
+                Console.WriteLine("Ingresa número: ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Datos del arreglo: ");
+            Console.WriteLine();
+            for (int i = 0; i < tam; i++) {
+                if (num[i] % 2 == 0)
+                {
+                    cp++;
+                }
+                else 
+                {
+                    cip++;
+                }
+                Console.WriteLine(num[i] + ", ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Cantidad de pares: {cp}");
+            Console.WriteLine($"Cantida de impares: {cip}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practica2/Practica2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tabla1 = new int[3, 3];
+            int f, c;
+            double suma;
+            Console.WriteLine("Programa usando arreglos bidimensionales");
+            Console.WriteLine("Digita los datos del arreglo");
+            //Llenar el arreglo
+            for (f = 0; f < 3; f++)
+            {
+                for (c = 0; c < 3; c++)
+                {
+                    tabla1[f, c] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("");
+            //Imprimir el arreglo
+            Console.WriteLine("Imprimiendo el arreglo...");
+            for (f = 0; f < 3; f++)
+            {
+                for (c = 0; c < 3; c++)
+                {
+                    Console.Write(" " + tabla1 [ f , c ] );
+                } 
+                Console.WriteLine("");
+                suma = f + c;
+            }
+            Console.WriteLine(value: $"La suma del arreglo es:{suma}");
+            Console.ReadKey();
+        }
+    }
+}
